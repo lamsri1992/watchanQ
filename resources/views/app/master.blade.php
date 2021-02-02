755b546d458020d5d4d9dd9863e1166a1866b326
satiya3577@gmail.com <satiya3577@gmail.com>
2021-02-02 09:35:51 +0700

    first commit

---
+++ resources/views/app/master.blade.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+    <style>
+        body {
+            padding-top: 4.5rem;
+        }
+    </style>
+<body>
+    @include('app.nav')
+    <main role="main" class="flex-shrink-0">
+        <div id="app">
+            <div class="container-fluid">
+                @section('content')
+                @show
+            </div>
+        </div>
+    </main>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+<script src="{{ asset('vendor/autoscroll/autoscroll.js') }}"></script>
+@section('script')
+@show
+</head>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Watchan Smart Hospital</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;300&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Prompt', sans-serif;
+        }
+
+        .full-height {
+            height: 30vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 16px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Watchan Smart Hospital
+            </div>
+            <div class="links">
+                <a href="/monitor/er">ห้องฉุกเฉิน</a>
+                <a href="#">ผู้ป่วยนอก</a>
+                <a href="#">ห้องตรวจ 1</a>
+                <a href="#">ห้องตรวจ 2</a>
+                <a href="#">ห้องตรวจ 3</a>
+                <a href="#">ทันตกรรม</a>
+                <a href="#">เภสัชกรรม</a>
+                <a href="#">คลินิค</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
