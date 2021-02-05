44498e7cd661ac19cd251820155f29bb99630c8a
satiya3577@gmail.com <satiya3577@gmail.com>
2021-02-05 11:59:08 +0700

    Update OPD Slide Size

--- resources/views/monitor/opd.blade.php
+++ resources/views/monitor/opd.blade.php
@@ -31,16 +31,16 @@
                 <div id="opdSlide" class="carousel slide" style="background-color: #343a40" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-interval="10000">
-                            <img src="http://www.prdmh.com/images/Covid-19/%E0%B8%AD%E0%B8%94_%E0%B8%AE%E0%B8%94_%E0%B8%AA.jpg" width="75%">
+                            <img src="http://www.prdmh.com/images/Covid-19/%E0%B8%AD%E0%B8%94_%E0%B8%AE%E0%B8%94_%E0%B8%AA.jpg" width="480px" height="480px">
                         </div>
                         <div class="carousel-item" data-interval="10000">
-                            <img src="http://www.prdmh.com/images/Covid-19/%E0%B9%80%E0%B8%8A%E0%B8%84%E0%B8%AD%E0%B8%A2%E0%B8%B2%E0%B8%87%E0%B9%84%E0%B8%A3_%E0%B8%82%E0%B8%B2%E0%B8%A7%E0%B9%84%E0%B8%AB%E0%B8%99%E0%B8%88%E0%B8%A3%E0%B8%87%E0%B8%9B%E0%B8%A5%E0%B8%AD%E0%B8%A1.jpg" width="75%">
+                            <img src="http://www.prdmh.com/images/Covid-19/%E0%B9%80%E0%B8%8A%E0%B8%84%E0%B8%AD%E0%B8%A2%E0%B8%B2%E0%B8%87%E0%B9%84%E0%B8%A3_%E0%B8%82%E0%B8%B2%E0%B8%A7%E0%B9%84%E0%B8%AB%E0%B8%99%E0%B8%88%E0%B8%A3%E0%B8%87%E0%B8%9B%E0%B8%A5%E0%B8%AD%E0%B8%A1.jpg" width="480px" height="480px">
                         </div>
                         <div class="carousel-item" data-interval="10000">
-                            <img src="http://www.prdmh.com/images/Covid-19/%E0%B8%A3%E0%B8%9E%E0%B8%88.%E0%B8%82%E0%B8%AD%E0%B8%99%E0%B9%81%E0%B8%81%E0%B8%99_info_4_%E0%B8%A0%E0%B8%B2%E0%B8%84_%E0%B9%80%E0%B8%AB%E0%B8%99%E0%B8%AD.jpg" width="75%">
+                            <img src="http://www.prdmh.com/images/Covid-19/%E0%B8%A3%E0%B8%9E%E0%B8%88.%E0%B8%82%E0%B8%AD%E0%B8%99%E0%B9%81%E0%B8%81%E0%B8%99_info_4_%E0%B8%A0%E0%B8%B2%E0%B8%84_%E0%B9%80%E0%B8%AB%E0%B8%99%E0%B8%AD.jpg" width="480px" height="480px">
                         </div>
                         <div class="carousel-item" data-interval="10000">
-                            <img src="http://www.prdmh.com/images/Covid-19/3%E0%B9%84%E0%B8%A13%E0%B8%95%E0%B8%AD%E0%B8%87_%E0%B9%82%E0%B8%84%E0%B8%A7%E0%B8%9419-01.jpg" width="75%">
+                            <img src="http://www.prdmh.com/images/Covid-19/3%E0%B9%84%E0%B8%A13%E0%B8%95%E0%B8%AD%E0%B8%87_%E0%B9%82%E0%B8%84%E0%B8%A7%E0%B8%9419-01.jpg" width="480px" height="480px">
                         </div>
                     </div>
                 </div>
