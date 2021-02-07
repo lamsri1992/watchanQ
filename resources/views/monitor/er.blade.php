1363bb807839622635ef61b601812fd61d41fd27
satiya3577@gmail.com <satiya3577@gmail.com>
2021-02-07 19:15:16 +0700

    Update ER Monitor

--- resources/views/monitor/er.blade.php
+++ resources/views/monitor/er.blade.php
@@ -20,11 +20,17 @@
     }
 }
 </style>
+@php
+    $data = count($result); $data_refresh = ($data * 3) * 1000;
+    if($data_refresh == 0){ $data_settime = 10000; }else{ $data_settime=$data_refresh; }
+    // echo $data_settime;
+@endphp
 <div class="row">
     <div class="col-md-12">
         <div class="card-body text-center" style="background-color:#17a2b8;border-radius: 25px;">
             <h1 class="card-title" style="font-size:45px;color:white;"><i class="fa fa-user-injured"></i>
-                <strong>คิวรอตรวจห้องฉุกเฉิน</strong>
+                <strong>คิวรอตรวจห้องฉุกเฉิน</strong> : 
+                <span class="badge badge-pill badge-danger" style="font-size: 40px;">{{ $data }}</span> คิว
             </h1>
             <div id="liveQ">
                 <div class="AutoScroll scroller tableFixHead" id="id-1" data-config='{"delay" : 5000 , "amount" : 100}'>
@@ -93,12 +99,6 @@
         </div>
     </div>
 </div>
-
-@php
-    $data = count($result); $data_refresh = ($data * 3) * 1000;
-    if($data_refresh == 0){ $data_settime = 10000; }else{ $data_settime=$data_refresh; }
-    echo $data_settime;
-@endphp
 
 @endsection
 @section('script')
